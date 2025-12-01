ecce6171e208ce9941118ee1aa5adbde1361f648
HK33M <112273747+hk33m@users.noreply.github.com>
2025-12-02 00:17:00 +0300

    Update home.jsx

--- src/home.jsx
+++ src/home.jsx
@@ -8,12 +8,12 @@ const [name,setname]=useState(()=>{
     const n=localStorage.getItem("name");
     return n!==null ? JSON.parse(n) : "" 
 });
-
+const answer = localStorage.getItem("answers");
   const handelbutton = ()=>{
         localStorage.setItem("name",JSON.stringify(name));
         navigate("/question");
    }
-const answer = 1;
+
     return(
         <div className="min-h-screen flex justify-center items-center bg-gray-100 dark:bg-slate-900 " dir="rtl">
              <div className="w-[400px] md:w-[600px] bg-white dark:bg-slate-800 shadow-2xl p-8 rounded-2xl text-center">
@@ -50,7 +50,7 @@ const answer = 1;
          whileTap={{ scale: 0.9 }}
          onClick={handelbutton}
          disabled={name==""}
-        className="w-full cursor-pointer bg-blue-600 hover:bg-blue-700 text-white py-3 rounded-xl text-lg shadow-lg disabled:bg-gray-300 disabled:cursor-not-allowed">{answer==1 ? "إكمال الاسئلة" : "بدء الاسئلة"} </motion.button>
+        className="w-full cursor-pointer bg-blue-600 hover:bg-blue-700 text-white py-3 rounded-xl text-lg shadow-lg disabled:bg-gray-300 disabled:cursor-not-allowed">{answer!==null ? "إكمال الاسئلة" : "بدء الاسئلة"} </motion.button>
         </motion.div>
         </AnimatePresence>
         </div>
